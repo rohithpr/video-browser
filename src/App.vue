eca2adb8a324632c0162912aa8a2c70439219634
Rohith <praroh2@gmail.com>
2019-08-10 21:38:24 +0530

    Add search bar
    Note: Remove hardcoded api key and disable it from google dashboard before pushing the project to github

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,33 @@
-import Vue from 'vue';
-
 <template>
-    <div>App</div>
+    <div>
+        <SearchBar @termChange="onTermChange"></SearchBar>
+    </div>
 </template>
 
 <script>
+import SearchBar from "./components/SearchBar";
+import axios from "axios";
+const API_KEY = "AIzaSyC51FJNBIOCT5pZbMYsqMy_6I4dzKiwprw";
+
 export default {
-    name: "App"
+    name: "App",
+    components: {
+        SearchBar
+    },
+    methods: {
+        onTermChange(searchTerm) {
+            axios
+                .get("https://www.googleapis.com/youtube/v3/search", {
+                    params: {
+                        key: API_KEY,
+                        type: "video",
+                        part: "snippet",
+                        q: searchTerm
+                    }
+                })
+                .then(response => console.log(response));
+        }
+    }
 };
 </script>
 
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <input @input="onInput" />
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        onInput(event) {
+            this.$emit("termChange", event.target.value);
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
